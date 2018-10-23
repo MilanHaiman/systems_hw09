674de2f2fb6305acc6d08b967931e43cb920dfc7
MilanHaiman <mhaiman@stuy.edu>
2018-10-23 10:48:38 -0400

    open, close

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #include <unistd.h>
 
 int main() {
-	int filedescriptor = open("intput.c", O_RDONLY);
+	int filedescriptor = open("intput.txt", O_RDONLY);
 	// printf("%s\n", ); // test reading the file
 	close(filedescriptor);
 
-	int filedescriptor2 = open("output.c", O_WRONLY);
+	int filedescriptor2 = open("output.txt", O_WRONLY);
 	// test writing to the file
 	close(filedescriptor2);
 }
