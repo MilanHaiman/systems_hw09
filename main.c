0d86173de62d484f30e6c64e7fa938f85baaac4c
MilanHaiman <mhaiman@stuy.edu>
2018-10-23 15:26:08 -0400

    print statements

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 #include <unistd.h>
 
 int main() {
+	printf("Testing open(), close(), read(), write().\nWe will read from input.txt and write to output.txt.\n\n");
 	int filedescriptor = open("input.txt", O_RDONLY);
 	// test reading the file
 	char buf[100]; 
 	int bytes_read;
 	bytes_read=read(filedescriptor,buf,11);
 	buf[bytes_read]='\0';
-	printf("%s\n",buf ); 
+	printf("Text read from input.txt:\n%s\n\n",buf );
 	close(filedescriptor);
 
 	int filedescriptor2 = open("output.txt", O_WRONLY);
@@ -19,5 +20,6 @@ int main() {
 	char * buf1="This is an example of write\n";
 	int bytes_wrote;
 	bytes_wrote=write(filedescriptor2, buf1,strlen(buf1));
+	printf("Text written to output.txt:\n%s\n\n",buf1 );
 	close(filedescriptor2);
 }
