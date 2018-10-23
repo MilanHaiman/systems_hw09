cff7a755a87151a3a9dbf4a5adc5cbfd38e8daef
Zer0-M <mjamil90@stuy.edu>
2018-10-23 11:43:39 -0400

    added tests for read and write

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int main() {
-	int filedescriptor = open("intput.txt", O_RDONLY);
-	// printf("%s\n", ); // test reading the file
+	int filedescriptor = open("input.txt", O_RDONLY);
+	// test reading the file
+	char buf[100]; 
+	int bytes_read;
+	bytes_read=read(filedescriptor,buf,11);
+	buf[bytes_read]='\0';
+	printf("%s\n",buf ); 
 	close(filedescriptor);
 
 	int filedescriptor2 = open("output.txt", O_WRONLY);
 	// test writing to the file
+	char * buf1="This is an example of write\n";
+	int bytes_wrote;
+	bytes_wrote=write(filedescriptor2, buf1,strlen(buf1));
 	close(filedescriptor2);
 }
